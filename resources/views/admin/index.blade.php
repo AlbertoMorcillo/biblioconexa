02d6e4eab4cc417562a67578e7169bcf4abb9f5c
Alberto <a.morcillo@sapalomera.cat>
2024-05-17 17:36:16 +0200

    añadido imagenes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -27,6 +27,7 @@
             <div class="col">
                 <div class="card shadow-sm">
                     <a href="{{ route('admin.usuarios') }}" class="link-libros" aria-label="Gestión de usuarios">
+                        <img class="card-img-top" src="{{ asset('images/admin/usuarios.jpg') }}" alt="Imagen de la portada del libro" width="100%" height="225" />
                         <div class="card-body">
                             <h5 class="card-title titulo-libro" aria-label="Gestión de usuarios">Usuarios</h5>
                         </div>
@@ -35,25 +36,8 @@
             </div>
             <div class="col">
                 <div class="card shadow-sm">
-                    <a href="{{ route('admin.libros') }}" class="link-libros" aria-label="Gestión de libros">
-                        <div class="card-body">
-                            <h5 class="card-title titulo-libro" aria-label="Gestión de libros">Libros</h5>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card shadow-sm">
-                    <a href="{{ route('admin.categorias') }}" class="link-libros" aria-label="Gestión de categorías">
-                        <div class="card-body">
-                            <h5 class="card-title titulo-libro" aria-label="Gestión de categorías">Categorías</h5>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card shadow-sm">
                     <a href="{{ route('admin.noticias') }}" class="link-libros" aria-label="Gestión de noticias">
+                        <img class="card-img-top" src="{{ asset('images/admin/noticias.jpg') }}" alt="Imagen de la portada del libro" width="100%" height="225" />
                         <div class="card-body">
                             <h5 class="card-title titulo-libro" aria-label="Gestión de noticias">Noticias</h5>
                         </div>
@@ -63,22 +47,24 @@
             <div class="col">
                 <div class="card shadow-sm">
                     <a href="{{ route('admin.eventos') }}" class="link-libros" aria-label="Gestión de eventos">
+                        <img class="card-img-top" src="{{ asset('images/admin/eventos.jpg') }}" alt="Imagen de la portada del libro" width="100%" height="225" />
                         <div class="card-body">
                             <h5 class="card-title titulo-libro" aria-label="Gestión de eventos">Eventos</h5>
                         </div>
                     </a>
                 </div>
             </div>
-            <div class="col">
+        </div>
+        <div class="col">
                 <div class="card shadow-sm">
                     <a href="{{ route('admin.comentarios') }}" class="link-libros" aria-label="Gestión de comentarios">
+                        <img class="card-img-top" src="{{ asset('images/admin/comentarios.webp') }}" alt="Imagen de la portada del libro" width="100%" height="225" />
                         <div class="card-body">
                             <h5 class="card-title titulo-libro" aria-label="Gestión de comentarios">Comentarios</h5>
                         </div>
                     </a>
                 </div>
             </div>
-        </div>
     </section>
 </div>
 @endsection

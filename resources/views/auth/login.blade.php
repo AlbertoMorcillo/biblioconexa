17a7b9c6013effcf8527d22e10d5ffdaf775d060
Alberto <a.morcillo@sapalomera.cat>
2024-05-29 12:52:59 +0200

    quitado el boton de iniciar sesion con google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,13 +49,13 @@
             <a href="{{ route('password.request') }}" class="mb-3 d-block text-center">¿Has olvidado tu contraseña?</a>
         @endif
         <hr>
-        <p class="mt-2 mb-3 text-center">O inicia sesión con</p>
+        {{-- <p class="mt-2 mb-3 text-center">O inicia sesión con</p>
         <div class="d-flex justify-content-center">
             <button class="btn btn-google" type="button" aria-label="Botón que te hará iniciar sesión con Google">
                 <img class="google-logo" src="{{ asset('images/logos/google-logo.png') }}" alt="Logo de Google">
                 Iniciar sesión con Google
             </button>
-        </div>
+        </div> --}}
         <!-- Omitimos el inicio de sesión con Google a menos que esté implementado -->
 
         <p class="mt-5 mb-3 text-body-secondary">© BiblioConexa 2024</p>
